75f795a1e17b0fff3bd6337264e32919988db5d7
zhangliangyu <1376030056@qq.com>
2021-10-23 15:00:31 +0800

    tijia

--- src/vuePdfSss.vue
+++ src/vuePdfSss.vue
@@ -4,7 +4,7 @@
 	import componentFactory from './componentFactory'
 	import pdfjsWrapper from './pdfjsWrapper'
 
-	var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+	var PDFJS = require('./pdfjs-dist/es5/build/pdf.js');
 
 	if ( process.env.VUE_ENV !== 'server' ) {
 
